0e63b32ffdb17c861cb22e113cbf72ccac6b89d0
Uri Berger <uriber@cs.huji.ac.il>
2023-08-14 15:16:56 +0300

    Added translation based training

---
+++ reformulation_experiment/prepare_translation_training_data.py
@@ -0,0 +1,23 @@
+import defaultdict
+import json
+import sys
+import random
+
+assert len(sys.argv) == 2
+exp_ind = int(sys.argv[1])
+
+with open(f'reformulation_experiment/data/image_ids/additional_train_image_ids_{exp_ind}.json', 'r') as fp:
+    image_ids = json.load(fp)
+image_ids_dict = {x: True for x in image_ids}
+
+with open('reformulation_experiment/data/translated_data/flickr30k_zh_translated_helsinki.json', 'r') as fp:
+    translated_data = json.load(fp)
+image_id_to_captions = defaultdict(list)
+for x in translated_data:
+    image_id_to_captions[x['image_id']].append(x['caption'])
+
+
+with open(f'reformulation_experiment/data/translated_train_data/translated_train_data_{exp_ind}.txt', 'w') as fp:
+    for image_id, captions in image_id_to_captions.items():
+        if image_id in image_ids_dict:
+            cur_fp.write(f'{image_id}\t{random.choice(captions)}\n')
