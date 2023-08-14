73ae79e98ccde833a05e3a652d5e72c8c89dc389
Uri Berger <uriber@cs.huji.ac.il>
2023-08-14 18:00:51 +0300

    Filtered image ids to only include those that are in the original flickr30k dataset

--- reformulation_experiment/utils.py
+++ reformulation_experiment/utils.py
@@ -1,6 +1,11 @@
 from collections import defaultdict
+import json
 
 def get_flickr_captions():
+    with open('/cs/labs/oabend/uriber/datasets/flickr30/karpathy/dataset_flickr30k.json', 'r') as fp:
+        flickr_data = json.load(fp)['images']
+    image_ids_dict = {int(x['filename'].split('.jpg')[0]): True for x in flickr_data}
+    
     with open('datasets/flickr_caption.txt', 'r') as f:
         lines = f.readlines()
     image_id_to_captions = defaultdict(list)
@@ -9,6 +14,7 @@ def get_flickr_captions():
         line = lines[i].strip()
         image_id, caption = line.split('\t')
         image_id = int(image_id)
-        image_id_to_captions[image_id].append(caption)
+        if image_id in image_ids_dict:
+            image_id_to_captions[image_id].append(caption)
 
     return image_id_to_captions
