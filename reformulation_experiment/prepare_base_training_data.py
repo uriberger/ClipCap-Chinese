815f19a9f9b8c5d20a98c64f34c18e58a7160a6c
Uri Berger <uriber@cs.huji.ac.il>
2023-08-16 00:30:01 +0300

    Added captioning models based training

--- reformulation_experiment/prepare_base_training_data.py
+++ reformulation_experiment/prepare_base_training_data.py
@@ -1,27 +1,30 @@
 import json
 import sys
 import random
-from utils import get_flickr_captions
+from utils import get_flickr8kcn_data, get_flickr30k_data
 
 assert len(sys.argv) == 3
 exp_ind = int(sys.argv[1])
 base_train_sample_num = int(sys.argv[2])
 
-image_id_to_captions = get_flickr_captions()
+flickr8kcn_data = get_flickr8kcn_data()
 
-print(f'Collected {len(image_id_to_captions)} images and {sum([len(x) for x in image_id_to_captions.values()])} captions')
+print(f'Collected {len(flickr8kcn_data)} samples')
 
-image_ids = list(image_id_to_captions.keys())
+flickr8kcn_image_ids = list(set([x['image_id'] for x in flickr8kcn_data]))
+base_image_ids = random.sample(flickr8kcn_image_ids, base_train_sample_num)
+base_image_ids_dict = {x: True for x in base_image_ids}
+
+flickr30k_data = get_flickr30k_data()
+all_image_ids = [int(x['filename'].split('.jpg')[0]) for x in flickr30k_data]
+flickr8kcn_image_ids_dict = {x: True for x in flickr8kcn_image_ids}
+non_flickr8kcn_image_ids = [x for x in all_image_ids if x not in flickr8kcn_image_ids_dict]
 
 test_image_num = 1000
-test_image_ids = random.sample(image_ids, test_image_num)
+test_image_ids = random.sample(non_flickr8kcn_image_ids, test_image_num)
 test_image_dict = {x: True for x in test_image_ids}
 
-train_image_ids = [x for x in image_ids if x not in test_image_dict]
-base_image_ids = random.sample(train_image_ids, base_train_sample_num)
-
-base_image_ids_dict = {x: True for x in base_image_ids}
-additional_image_ids = [x for x in train_image_ids if x not in base_image_ids_dict]
+additional_image_ids = [x for x in all_image_ids if x not in base_image_ids_dict and x not in test_image_dict]
 additional_image_ids_dict = {x: True for x in additional_image_ids}
 
 with open(f'reformulation_experiment/data/image_ids/base_train_image_ids_{exp_ind}.json', 'w') as fp:
@@ -32,17 +35,7 @@ with open(f'reformulation_experiment/data/image_ids/test_image_ids_{exp_ind}.jso
     fp.write(json.dumps(test_image_ids))
 print(f'Selected {len(base_image_ids)} base train image ids, {len(additional_image_ids)} additional train image ids and {len(test_image_ids)} test image ids')
 
-base_train_fp = open(f'reformulation_experiment/data/base_train_data/base_train_data_{exp_ind}.txt', 'w')
-additional_train_fp = open(f'reformulation_experiment/data/gt_train_data/gt_train_data_{exp_ind}.txt', 'w')
-for image_id, captions in image_id_to_captions.items():
-    cur_fp = None
-    if image_id in base_image_ids_dict:
-        cur_fp = base_train_fp
-    elif image_id in additional_image_ids_dict:
-        cur_fp = additional_train_fp
-        captions = random.sample(captions, 1)
-    if cur_fp is not None:
-        for caption in captions:
-            cur_fp.write(f'{image_id}\t{caption}\n')
-base_train_fp.close()
-additional_train_fp.close()
+with open(f'reformulation_experiment/data/base_train_data/base_train_data_{exp_ind}.txt', 'w') as fp:
+    for sample in flickr8kcn_data:
+        if sample['image_id'] in base_image_ids_dict:
+            fp.write(f'{sample["image_id"]}\t{sample["caption"]}\n')
--- reformulation_experiment/utils.py
+++ reformulation_experiment/utils.py
@@ -1,20 +1,27 @@
-from collections import defaultdict
 import json
+import os
+from config import flickr30k_path, flickr8kcn_root
 
-def get_flickr_captions():
-    with open('/cs/labs/oabend/uriber/datasets/flickr30/karpathy/dataset_flickr30k.json', 'r') as fp:
+def get_flickr8kcn_data():
+    data_dir_name = 'data'
+    caption_file_name = 'flickr8kzhc.caption.txt'
+    caption_file_path = os.path.join(flickr8kcn_root, data_dir_name, caption_file_name)
+
+    image_id_captions_pairs = []
+    with open(caption_file_path, 'r', encoding='utf8') as fp:
+        for line in fp:
+            striped_line = line.strip()
+            if len(striped_line) == 0:
+                continue
+            line_parts = striped_line.split()
+            assert len(line_parts) == 2
+            image_id = int(line_parts[0].split('_')[0])
+            caption = line_parts[1]
+            image_id_captions_pairs.append({'image_id': image_id, 'caption': caption})
+            
+    return image_id_captions_pairs
+
+def get_flickr30k_data():
+    with open(flickr30k_path, 'r') as fp:
         flickr_data = json.load(fp)['images']
-    image_ids_dict = {int(x['filename'].split('.jpg')[0]): True for x in flickr_data}
-    
-    with open('datasets/flickr_caption.txt', 'r') as f:
-        lines = f.readlines()
-    image_id_to_captions = defaultdict(list)
-
-    for i in range(len(lines)):
-        line = lines[i].strip()
-        image_id, caption = line.split('\t')
-        image_id = int(image_id)
-        if image_id in image_ids_dict:
-            image_id_to_captions[image_id].append(caption)
-
-    return image_id_to_captions
+    return flickr_data
