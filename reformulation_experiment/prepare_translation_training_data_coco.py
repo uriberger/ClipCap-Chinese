b292e675d52765a2e9a70c311c5860111d2cd24d
Uri Berger <uriber@cs.huji.ac.il>
2023-08-20 17:06:26 +0300

    Fixed some bugs in translation data preperation

--- reformulation_experiment/prepare_translation_training_data_coco.py
+++ reformulation_experiment/prepare_translation_training_data_coco.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 import json
 import sys
 import random
+import pickle
 
 assert len(sys.argv) == 2
 exp_ind = int(sys.argv[1])
@@ -12,10 +13,10 @@ with open('reformulation_experiment/data/translated_data/coco_zh_translated_hels
     coco_translated_data = json.load(fp)
 with open(f'reformulation_experiment/data/base_train_data/additional_train_data_{exp_ind}.json', 'r') as fp:
     additional_train_data = json.load(fp)
-with open(f'reformulation_experiment/data/image_ids/flickr_orig_to_new_image_id_{exp_ind}.json', 'r') as fp:
-    flickr_image_ids_dict = json.load(fp)
-with open(f'reformulation_experiment/data/image_ids/coco_orig_to_new_image_id_{exp_ind}.json', 'w') as fp:
-    coco_image_ids_dict = json.load(fp)
+with open(f'reformulation_experiment/data/image_ids/flickr_orig_to_new_image_id_{exp_ind}.pkl', 'rb') as fp:
+    flickr_image_ids_dict = pickle.load(fp)
+with open(f'reformulation_experiment/data/image_ids/coco_orig_to_new_image_id_{exp_ind}.pkl', 'rb') as fp:
+    coco_image_ids_dict = pickle.load(fp)
 
 image_id_to_path = {i: additional_train_data[i]['file_path'] for i in range(len(additional_train_data))}
 
@@ -28,7 +29,7 @@ for x in coco_translated_data:
         image_id_to_captions[coco_image_ids_dict[x['image_id']]].append(x['caption'])
 
 train_data = []
-for image_id, captions in image_id_to_data.items():
+for image_id, captions in image_id_to_captions.items():
     train_data.append({'image_id': image_id, 'image_path': image_id_to_path[image_id], 'caption': random.choice(captions)})
 
 with open(f'reformulation_experiment/data/translated_train_data/translated_train_data_{exp_ind}.json', 'w') as fp:
