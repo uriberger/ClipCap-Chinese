0fb270083159c3d1fc21a5b3fb0e85adc8d2b2ab
Uri Berger <uri.berger2@mail.huji.ac.il>
2023-08-20 13:35:28 +0300

    Disabled caching of dataset

--- dataset.py
+++ dataset.py
@@ -16,52 +16,40 @@ class ClipCapDataset(Dataset):
         assert mode in ['train', 'test']
         self.normalize_prefix = normalize_prefix
         pad_id = tokenizer.pad_token_id
-        if mode == 'train':
-            save_path = join(os.path.dirname(clip_data_path), 'train.pkl')
-        else:
-            save_path = join(os.path.dirname(clip_data_path), 'test.pkl')
 
-        # 加载缓存
-        if os.path.isfile(save_path):
-            with open(save_path, 'rb') as f:
-                self.clip_embeds, self.caption_ids_list, self.mask_list = pickle.load(f)
-            logger.info('num of training data'.format(len(self.clip_embeds)))
-        else:
-            logger.info('loading dataset:{}'.format(clip_data_path))
-            with open(clip_data_path, 'rb') as f:
-                caption_list, image_id2embed = pickle.load(f)
-            logger.info('num of image embedding:{}'.format(len(image_id2embed)))
-            logger.info('num of captions:{}'.format(len(caption_list)))
+        logger.info('loading dataset:{}'.format(clip_data_path))
+        with open(clip_data_path, 'rb') as f:
+            caption_list, image_id2embed = pickle.load(f)
+        logger.info('num of image embedding:{}'.format(len(image_id2embed)))
+        logger.info('num of captions:{}'.format(len(caption_list)))
 
-            clip_embeds = []
-            caption_ids_list = []
-            mask_list = []
-            for image_id, caption in caption_list:
-                clip_embed = image_id2embed[image_id].squeeze(0).float()
-                caption_ids = tokenizer.encode(caption, add_special_tokens=False)
-                caption_ids.append(tokenizer.sep_token_id)
+        clip_embeds = []
+        caption_ids_list = []
+        mask_list = []
+        for image_id, caption in caption_list:
+            clip_embed = image_id2embed[image_id].squeeze(0).float()
+            caption_ids = tokenizer.encode(caption, add_special_tokens=False)
+            caption_ids.append(tokenizer.sep_token_id)
 
-                # truncate
-                caption_ids = caption_ids[:max_len-prefix_len]
-                mask = [1] * (prefix_len + len(caption_ids))
+            # truncate
+            caption_ids = caption_ids[:max_len-prefix_len]
+            mask = [1] * (prefix_len + len(caption_ids))
 
-                # padding
-                padding_len = max_len - prefix_len - len(caption_ids)
-                caption_ids += [pad_id]*padding_len
-                mask += [0]*padding_len
+            # padding
+            padding_len = max_len - prefix_len - len(caption_ids)
+            caption_ids += [pad_id]*padding_len
+            mask += [0]*padding_len
 
-                caption_ids = torch.tensor(caption_ids).long()
-                mask = torch.tensor(mask).long()
+            caption_ids = torch.tensor(caption_ids).long()
+            mask = torch.tensor(mask).long()
 
-                clip_embeds.append(clip_embed)
-                caption_ids_list.append(caption_ids)
-                mask_list.append(mask)
-            with open(save_path, 'wb') as f:
-                pickle.dump([clip_embeds, caption_ids_list, mask_list], f)
-            self.clip_embeds = clip_embeds
-            self.caption_ids_list = caption_ids_list
-            self.mask_list = mask_list
-            logger.info('num of training data'.format(len(self.clip_embeds)))
+            clip_embeds.append(clip_embed)
+            caption_ids_list.append(caption_ids)
+            mask_list.append(mask)
+        self.clip_embeds = clip_embeds
+        self.caption_ids_list = caption_ids_list
+        self.mask_list = mask_list
+        logger.info('num of training data'.format(len(self.clip_embeds)))
 
     def __len__(self) -> int:
         return len(self.caption_ids_list)
